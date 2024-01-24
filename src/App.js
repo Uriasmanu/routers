d7409d09d6d385160767a5cd2d9841847cea16e0
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-01-24 12:28:17 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Inicio from "./paginas/inicio";
 import Sobre from "./paginas/sobre";
+import Menu from './paginas/menu';
 
 function App() {
   return (
     <BrowserRouter>
+    <Menu/>
     <Routes>
       <Route path="/" element={<Inicio />} />
       <Route path="/sobremim" element={<Sobre />} />
+      <Route path="*" element={"Pagina não encontrada"} />
+
     </Routes>
     </BrowserRouter>
   )
